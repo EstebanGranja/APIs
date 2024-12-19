e71fda33afe51c1232b8863b3fce40ecc2276049
unknown <estebangranja@mi.unc.edu.ar>
2024-12-19 20:21:50 -0300

    to do list options

--- OpenStreetMap API (to do list)/app.py
+++ OpenStreetMap API (to do list)/app.py
@@ -1,12 +1,46 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__, template_folder="templates")
 
-tasks = [{"task1": "Go shopping", "done": False}]
+tasks = []
 
 @app.route('/')
 def home():
 	return render_template("index.html", to_do=tasks)
+
+
+
+@app.route("/add", methods=["POST"])
+def add_task():
+    task = request.form['to_do_form']
+    tasks.append({"task": task, "done": False}) # Set task not done by default
+    return redirect(url_for("home"))  
+
+
+
+@app.route("/edit/<int:index>", methods=["GET", "POST"])
+def edit_task(index):
+    task = tasks[index]
+    if request.method == "GET":
+        return render_template("edit.html", to_do=tasks, index=index) # Edit template
+    else:
+        task["task"] = request.form["to_do_form"] # Update task
+        return redirect(url_for("home"))
+
+
+
+@app.route("/check/<int:index>")
+def check_task(index):
+    tasks[index]['done'] = not tasks[index]['done'] # Changes if task is done or not
+    return redirect(url_for("home"))
+
+
+
+@app.route("/delete/<int:index>")
+def delete_task(index):
+    del tasks[index]
+    return redirect(url_for("home"))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
